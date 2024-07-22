805c02e9063ec96622eb656d7c74f36fff5db7f7
Randid20 <142280765+Randid20@users.noreply.github.com>
2024-07-22 14:40:50 +0700

    Add files via upload

---
+++ pages/buku/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-tg-12">
+        <h2 class="text-center my-4">BUKU</h2>
+        <div class="my-3">
+          <input type="search" class="form-control rounded-5" placeholder="mau baca apa hari ini?">
+        </div>
+        <div class="my-3 text-muted">menampilkan 3 dari 3</div>
+        <div class="row">
+          <div class="col-lg-2">            
+         <div class="card mb-3">
+          <div class="card-body">
+            <img src="/assets/img/ega.jpeg" class="cover" alt ="cover 1">
+              
+          </div>
+        </div>
+       </div> 
+       <div class="col-lg-2">            
+         <div class="card mb-3">
+          <div class="card-body">
+            <img src="/assets/img/g.jpeg" class="cover" alt ="cover 1">
+              
+          </div>
+        </div>
+       </div> 
+       <div class="col-lg-2">            
+         <div class="card mb-3">
+          <div class="card-body">
+            <img src="/assets/img/laskar-pelangi.jpg" class="cover" alt ="cover 1">
+              
+          </div>
+        </div>
+       </div> 
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+<style scoped>
+.cover {
+  width: 100%;
+  height: 100%; 
+  object-fit: cover;
+  object-position: 0 30;
+}
+.form-control{
+  background-color: #A08D8D;
+}
+</style>
---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container-fluid">
+    <div class="col-lg-12">
+      <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+      <div class="my-3">
+        <input type="search" class="form-control from-control-lg rounded-5" placeholder="Filter...">
+      </div>
+      <div class="my-3 text-muted">menampilkan 3 dari 3</div>
+      <table class="table">
+        <thead>
+          <tr>
+            <td>#</td>
+            <td>NAMA</td>
+            <td>KEANGGOTAAN</td>
+            <td>WAKTU</td>
+            <td>KEPERLUAN</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1.</td>
+            <td>Randid Delvin</td>
+            <td>Siswa</td>
+            <td>27 February 2024, 23.31.00</td>
+            <td>Baca</td>
+          </tr>
+        </tbody>
+      </table>
+      <nuxt-link to="/">
+          <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">Kembali</button>
+        </nuxt-link>
+    </div>
+  </div>
+</template>
